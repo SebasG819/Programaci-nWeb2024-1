6df8aab98ebe3839c57d7e208e003f5dee39ed92
SebasG819 <sebasgonzalez819@gmail.com>
2024-02-16 11:27:50 -0500

    Desktop

--- LandingPage/src/Components/NavbarDesktop/NavbarDesktop.jsx
+++ LandingPage/src/Components/NavbarDesktop/NavbarDesktop.jsx
@@ -27,15 +27,17 @@ export function NavbarDesktop(){
             {NaviDesk.map((card) => (
                 
                 <nav className="NavigationBarDesktop">
-    
+                <div className='firspart-nav'>
                 {card.iconlogo && <img src={card.iconlogo} alt="Photo_Logo" className = "photo-logo" />}
                 <h1 className='text-nav'>{card.followtext}</h1>
                 <h1 className='text-nav'>{card.exploretext}</h1>
                 {card.iconmore && <img src={card.iconmore} alt="Photo_More" className = "icon" />}
+                </div>
                 <div className='searchbar'>
                 <input type={card.inputsearch} className = "input-search" />
                 {card.iconsearch && <img src={card.iconsearch} alt="Photo_Search" className = "icon" />}
                 </div >
+                <div className='lastpart-nav'>
                 {card.iconcrown && <img src={card.iconcrown} alt="Crown_Icon" className = "icon" />}
                 {card.iconmail && <img src={card.iconmail} alt="Mail_Icon" className = "icon" />}
                 {card.iconmessage && <img src={card.iconmessage} alt="Message_Icon" className = "icon" />}
@@ -44,7 +46,7 @@ export function NavbarDesktop(){
                 <button className="button-live"><img src={card.imagebutton} alt="Batery_Icon" className = "batery-icon"></img> {card.buttonText} </button>
                 </div>
                 {card.profileimg && <img src={card.profileimg} alt="Photo_Profile" className = "profile-navdesk" />}
-
+                </div>
                 
                 
                  </nav>
