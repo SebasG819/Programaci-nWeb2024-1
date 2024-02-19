4ea126aa36479b3e88a4862a61b083b676b42bf6
SebasG819 <sebasgonzalez819@gmail.com>
2024-02-19 12:39:34 -0500

    more desktop

--- LandingPage/src/Components/NavbarDesktop/NavbarDesktop.jsx
+++ LandingPage/src/Components/NavbarDesktop/NavbarDesktop.jsx
@@ -5,18 +5,18 @@ export function NavbarDesktop(){
 
     const NaviDesk = [{
 
-        iconlogo : "https://assets.stickpng.com/images/580b57fcd9996e24bc43c540.png",
+        iconlogo : "https://seeklogo.com/images/T/twitch-logo-4931D91F85-seeklogo.com.png",
         followtext: "Siguiendo",
         exploretext: "Explorar",
         iconmore: "https://cdn-icons-png.flaticon.com/512/149/149947.png",
         inputsearch: "Buscar",
         iconsearch: "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png",
-        iconcrown: "https://cdn.icon-icons.com/icons2/931/PNG/512/crown_icon-icons.com_72445.png",
+        iconcrown: "https://seeklogo.com/images/P/prime-gaming-logo-61A701B3F5-seeklogo.com.png",
         iconmail: "https://cdn.icon-icons.com/icons2/494/PNG/512/inbox_icon-icons.com_48285.png",
         iconmessage: "https://cdn.icon-icons.com/icons2/2596/PNG/512/message_unread_icon_155130.png",
         iconbits: "https://cdn-icons-png.freepik.com/512/8810/8810390.png",
         buttonText:"Mira sin anuncios",
-        imagebutton: "https://endlessicons.com/wp-content/uploads/2013/06/battery-icon-614x460.png",
+        imageprime: "https://endlessicons.com/wp-content/uploads/2013/06/battery-icon-614x460.png",
         profileimg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2IdvqEqsy_AAeceGATK3Ws8vjv_c9YcZf_TE5zvem2_686_qnZHiJMe1_b-lsxMwPKPA&usqp=CAU"
     }
     ]
@@ -43,9 +43,13 @@ export function NavbarDesktop(){
                 {card.iconmessage && <img src={card.iconmessage} alt="Message_Icon" className = "icon" />}
                 {card.iconbits && <img src={card.iconbits} alt="Bits_Icon" className = "icon" />}
                 <div className='button-photo'>
-                <button className="button-live"><img src={card.imagebutton} alt="Batery_Icon" className = "batery-icon"></img> {card.buttonText} </button>
-                </div>
+                <button className="button-prime">
+                <img src={card.imageprime} alt="Batery_Icon" className = "batery-icon"></img> 
+                {card.buttonText} 
+                </button>
                 {card.profileimg && <img src={card.profileimg} alt="Photo_Profile" className = "profile-navdesk" />}
+                </div>
+                
                 </div>
                 
                 
